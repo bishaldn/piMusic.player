e87711378f5b0f13e7ec59f082a976d816fed7fa
bishaldn <bdn0298@gmail.com>
2022-08-23 21:47:31 +0530

    adding changing color

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
+const container_color = document.getElementById('con');
 let isPlaying = false;
 const playMusic  = ()=>{
     isPlaying=true;
@@ -26,6 +27,18 @@ play.addEventListener('click',()=>{
     else{
         playMusic();
     }
+    setTimeout(() => {
+        setTimeout(() => {
+            setTimeout(() => {
+                setTimeout(() => {
+                    container_color.style.background = "linear-gradient(to right top, #181a21, #38264c, #791c5f, #bc004c, #e50610"; 
+                 },3000);
+               container_color.style.background = "linear-gradient(to right top, #0032e5, #382ad2, #4a23c0, #531dae, #58189d"; 
+            },3000);
+            container_color.style.background = "linear-gradient(to right top, #4931be, #a003a7, #d10087, #ec0064, #f64444";
+        },3000);
+        container_color.style.background = "linear-gradient(to right top, #a3e53b, #00d68b, #00b9e2, #0092ff, #1254eb";
+    }, 3000);
 })
 const songs = [
     {
@@ -51,11 +64,36 @@ songIndex = 0;
 const playNext=()=>{
     songIndex = (songIndex+1)%songs.length;
     songQueue(songs[songIndex]);
+    setTimeout(() => {
+        setTimeout(() => {
+            setTimeout(() => {
+                setTimeout(() => {
+                    container_color.style.background = "linear-gradient(to right top, #181a21, #38264c, #791c5f, #bc004c, #e50610"; 
+                 },3000);
+               container_color.style.background = "linear-gradient(to right top, #0032e5, #382ad2, #4a23c0, #531dae, #58189d"; 
+            },3000);
+            container_color.style.background = "linear-gradient(to right top, #4931be, #a003a7, #d10087, #ec0064, #f64444";
+        },3000);
+        container_color.style.background = "linear-gradient(to right top, #a3e53b, #00d68b, #00b9e2, #0092ff, #1254eb";
+    }, 3000);
 }
 
 const playPrev=()=>{
     songIndex = (songIndex-1)%songs.length;
     songQueue(songs[songIndex]);
+    setTimeout(() => {
+        setTimeout(() => {
+            setTimeout(() => {
+                setTimeout(() => {
+                    container_color.style.background = "linear-gradient(to right top, #181a21, #38264c, #791c5f, #bc004c, #e50610"; 
+                 },3000);
+               container_color.style.background = "linear-gradient(to right top, #0032e5, #382ad2, #4a23c0, #531dae, #58189d"; 
+            },3000);
+            container_color.style.background = "linear-gradient(to right top, #4931be, #a003a7, #d10087, #ec0064, #f64444";
+        },3000);
+        container_color.style.background = "linear-gradient(to right top, #a3e53b, #00d68b, #00b9e2, #0092ff, #1254eb";
+    }, 3000);
 }
 prev.addEventListener('click', playPrev);
 next.addEventListener('click',playNext);
+
